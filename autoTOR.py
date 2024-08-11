43337c8516db746705b2c0bc7e47dd6a1c54ecca
det0x3rr <det0x3rr@gmail.com>
2024-08-11 17:59:03 +0530

    Added some improvements and updated readme file

--- autoTOR.py
+++ autoTOR.py
@@ -2,85 +2,116 @@
 
 import time
 import os
-import subprocess
+import sys
+import requests
+
+sys.stdout.write("\033[0;32m")
 
 
-
-
-try:
-    check_pip3 = subprocess.check_output('dpkg -s python3-pip', shell=True)
-    if str('install ok installed') in str(check_pip3):
-        pass
-except subprocess.CalledProcessError:
-    print('[+] pip3 not installed')
-    subprocess.check_output('sudo apt update',shell=True)
-    subprocess.check_output('sudo apt install python3-pip -y', shell=True)
-    print('[!] pip3 installed succesfully')
-
-
-
-try:
-
-    import requests
-except Exception:
-    print('[+] python3 requests is not installed')
-    os.system('pip3 install requests')
-    os.system('pip3 install requests[socks]')
-    print('[!] python3 requests is installed ')
-try:
-
-    check_tor = subprocess.check_output('which tor', shell=True)
-except subprocess.CalledProcessError:
-
-    print('[+] tor is not installed !')
-    subprocess.check_output('sudo apt update',shell=True)
-    subprocess.check_output('sudo apt install tor -y',shell=True)
-    print('[!] tor is installed succesfully ')
-
-os.system("clear")
-def ma_ip():
-    url='https://www.myexternalip.com/raw'
-    get_ip= requests.get(url,proxies=dict(http='socks5://127.0.0.1:9050',https='socks5://127.0.0.1:9050'))
-    return get_ip.text
-
-def change():
-    os.system("service tor reload")
-    print ('[+] Your IP has been Changed to : '+str(ma_ip()))
-
-print('''\033[1;32;40m \n
+def initialize():
+    print('''
                 _          _______
      /\        | |        |__   __|
     /  \  _   _| |_ ___      | | ___  _ __
    / /\ \| | | | __/ _ \     | |/ _ \| '__|
   / ____ \ |_| | || (_) |    | | (_) | |
  /_/    \_\__,_|\__\___/     |_|\___/|_|
-                V 2.1
-from mrFD
+                V 2.2
+from det0x3rr
 ''')
-print("\033[1;40;31m http://facebook.com/ninja.hackerz.kurdish/\n")
+    print("Original RepoLink - https://github.com/FDX100/Auto_Tor_IP_changer")
+    print("Twitter Link - https://x.com/det0x3rr\n")
 
-os.system("service tor start")
+    os.system("service tor start")
+    print("change your  SOCKS to 127.0.0.1:9050 \n")
+
+
+def validate_user_input(user_input,defaultValue,):
+    if(len(user_input)==0):
+        return defaultValue
+    elif user_input.isdigit():
+        if int(user_input)<2:
+            if(defaultValue==0):
+                return user_input
+            else:
+                return defaultValue
+        return user_input
+    else:
+        print("Please enter only numbers...")
+        quit()
+
+
+def get_input():
+    try:
+        SECONDS_TO_CHANGE_IP = input("[+] Time to change Ip in seconds [default 2 second], Press enter for default >> ")
+        NO_OF_TIME_IP_WILL_CHANGE = input("[+] How many time do you want to change your ip [default 0 which is for infinite], Press enter for default >> ")
+
+        SECONDS_TO_CHANGE_IP = validate_user_input(SECONDS_TO_CHANGE_IP,2)
+        NO_OF_TIME_IP_WILL_CHANGE = validate_user_input(NO_OF_TIME_IP_WILL_CHANGE,0)
+
+        return SECONDS_TO_CHANGE_IP,NO_OF_TIME_IP_WILL_CHANGE
+
+    except KeyboardInterrupt:
+        quit()
+
+
+
+def get_my_ip():
+    url = "https://api.ipify.org/?format=text"
+    try:
+        get_ip= requests.get(url,proxies=dict(http='socks5h://127.0.0.1:9050',https='socks5h://127.0.0.1:9050'),timeout=10)
+        return get_ip.text
+
+    except requests.exceptions.ConnectionError:  
+        print("Connection Timeout, please check your internet connection...")
+        quit()
+
+
+ip_address_global=""
+def change_ip():
+    UP = "\033[1A" # it will take the cursor to previous line
+    CLEAR = "\x1b[2K" # it will clear the output of that line where the cursor presents
+    os.system("service tor reload")
+    ip_address = get_my_ip()
+    global ip_address_global
+    ip_address_global = ip_address
+    print(f"[+] Your IP has been Changed to : {ip_address}")
+    print(UP,end=CLEAR)
 
 
 
 
-time.sleep(3)
-print("\033[1;32;40m change your  SOCKES to 127.0.0.1:9050 \n")
-os.system("service tor start")
-x = input("[+] time to change Ip in Sec [type=60] >> ")
-lin = input("[+] how many time do you want to change your ip [type=1000]for infinte ip change type [0] >>")
-if int(lin) ==int(0):
+def main():
+    initialize()
+    SECONDS_TO_CHANGE_IP,NO_OF_TIME_IP_WILL_CHANGE = get_input()
 
-	while True:
-		try:
-			time.sleep(int(x))
-			change()
-		except KeyboardInterrupt:
+    print(f"\n[+] SECONDS_TO_CHANGE_IP = {SECONDS_TO_CHANGE_IP}")
+    if(NO_OF_TIME_IP_WILL_CHANGE==0):
+        print(f"[+] NO._OF_TIME_IP_WILL_CHANGE = inifinte\n")
+    else:
+        print(f"[+] NO_OF_TIME_IP_WILL_CHANGE = {NO_OF_TIME_IP_WILL_CHANGE} times\n")
 
-		 	print('\nauto tor is closed ')
-		 	quit()
 
-else:
-	for i in range(int(lin)):
-		    time.sleep(int(x))
-		    change()
+    def run():
+        try:
+            time.sleep(int(SECONDS_TO_CHANGE_IP))
+            change_ip()
+        except KeyboardInterrupt:
+            print(f"[+] Your IP has been Changed to : {ip_address_global}")
+            print('\nshutdowning.....')
+            print('Done')
+            os.system("service tor stop")
+            quit()
+
+    if int(NO_OF_TIME_IP_WILL_CHANGE) ==int(0):
+        while True:
+            run()
+    else:
+        for i in range(int(NO_OF_TIME_IP_WILL_CHANGE)):
+           run()
+        print(f"[+] Your IP has been Changed to : {ip_address_global}")          
+          
+
+    
+if __name__ == "__main__":
+    main()
--- install.py
+++ install.py
@@ -1,7 +1,40 @@
 import os
+import subprocess
+
 choice = input('[+] to install press (Y) to uninstall press (N) >> ')
 run = os.system
+
 if str(choice) =='Y' or str(choice)=='y':
+    # Checking pip is installed or not
+    try:
+        check_pip3 = subprocess.check_output('dpkg -s python3-pip', shell=True)
+        if str('install ok installed') in str(check_pip3):
+            pass
+    except subprocess.CalledProcessError:
+        print('[+] pip3 not installed')
+        subprocess.check_output('sudo apt update',shell=True)
+        subprocess.check_output('sudo apt install python3-pip -y', shell=True)
+        print('[!] pip3 installed succesfully')
+
+
+    # checking request is installed or not
+    try:
+        import requests
+    except Exception:
+        print('[+] python3 requests is not installed')
+        os.system('pip3 install requests')
+        os.system('pip3 install requests[socks]')
+        print('[!] python3 requests is installed ')
+
+
+    # checking tor is installed or not
+    try:
+        check_tor = subprocess.check_output('which tor', shell=True)
+    except subprocess.CalledProcessError:
+        print('[+] tor is not installed !')
+        subprocess.check_output('sudo apt update',shell=True)
+        subprocess.check_output('sudo apt install tor -y',shell=True)
+        print('[!] tor is installed succesfully ')
 
     run('chmod 777 autoTOR.py')
     run('mkdir /usr/share/aut')
@@ -11,8 +44,10 @@ if str(choice) =='Y' or str(choice)=='y':
     with open('/usr/bin/aut','w')as file:
         file.write(cmnd)
     run('chmod +x /usr/bin/aut & chmod +x /usr/share/aut/autoTOR.py')
-    print('''\n\ncongratulation auto Tor Ip Changer is installed successfully \nfrom now just type \x1b[6;30;42maut\x1b[0m in terminal ''')
-if str(choice)=='N' or str(choice)=='n':
+    print('''\n\ncongratulation auto Tor Ip Changer is installed successfully \nfrom now just type \x1b[6;30;42m sudo aut \x1b[0m in terminal ''')
+
+
+elif str(choice)=='N' or str(choice)=='n':
     run('rm -r /usr/share/aut ')
     run('rm /usr/bin/aut ')
     print('[!] now Auto Tor Ip changer  has been removed successfully')
